26b6e7f3b89927412c179bb29a504be0a398b3e9
sajustsmile <sajustsmile@live.com>
2022-02-03 00:56:47 +0100

    added styling to basket component

--- src/Header.js
+++ src/Header.js
@@ -7,46 +7,32 @@ import logo from './images/header_logo.png';
 
 function Header() {
   return <div className="header"> 
-  <img className="header_logo" src= { logo } />
-  <div className="header_search" >
-  <input className="header_searchInput" type="text" />
-  <SearchIcon className="header_searchIcon" />
-  </div>
+      <img className="header_logo" src= { logo } />
+      <div className="header_search" >
+      <input className="header_searchInput" type="text" />
+      <SearchIcon className="header_searchIcon" />
+    </div>
 
-  <div className="header_nav">
-  <div className="header_option">
-    <span className="header_optionLineOne">
-    Hello Guest
-    </span>
-    <span className="header_optionLineTwo">
-    Sign In
-    </span>
-  </div>
+    <div className="header_nav">
+      <div className="header_option">
+        <span className="header_optionLineOne">Hello Guest</span>
+        <span className="header_optionLineTwo">Sign In</span>
+      </div>
 
-  <div className="header_option">
-    <span className="header_optionLineOne">
-    Retruns
-    </span>
-    <span className="header_optionLineTwo">
-    & Orders
-    </span>
-  </div>
+      <div className="header_option">
+        <span className="header_optionLineOne">Retruns</span>
+        <span className="header_optionLineTwo">& Orders</span>
+      </div>
 
-  <div className="header_option">
-    <span className="header_optionLineOne">
-    Your
-    </span>
-    <span className="header_optionLineTwo">
-    Prime
-    </span>
-  </div>
-  <div className="header_optionBasket">
-  <ShoppingBasketIcon />
-  <span className="header_optionLineTwo header_basketCount">
-    0
-    </span>
-  </div>
-  </div>
+      <div className="header_option">
+        <span className="header_optionLineOne">Your</span>
+        <span className="header_optionLineTwo">Prime</span>
+      </div>
+      <div className="header_optionBasket">
+      <ShoppingBasketIcon />
+        <span className="header_optionLineTwo header_basketCount">0</span>
+      </div>
+    </div>
   </div>
 }
 
