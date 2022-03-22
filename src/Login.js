9755667850a642115b5e12267bc38ae048e0cbf2
sajustsmile <sajustsmile@live.com>
2022-03-22 20:32:36 +0100

    added sign in form

--- src/Login.js
+++ src/Login.js
@@ -10,6 +10,20 @@ function Login() {
             <img className='login_logo' 
             src={loginLogo} alt='' />
         </Link>
+        <div className='login_container'>
+            <h1>Sign-in</h1>
+            <form>
+                <h5>E-mail</h5>
+                <input type='text'/>
+                <h5>Password</h5>
+                <input type='password'/>
+                <button className='login_signInButton'>Sign In</button>
+            </form>
+            <p>By signing-in you agree to Prenzlauer's (AMAZON CLONE)
+                Conditions of Use & Sale. Please see our Privacy Notice,
+                our Cookies Notice and our Interest-Based Ads Notice.</p>
+        </div>
+        <button className='login_registerButton'>Create Your Prenzlauer Account</button>
     </div>
   )
 }
